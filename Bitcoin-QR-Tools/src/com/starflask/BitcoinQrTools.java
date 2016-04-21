cfe5bc6c3ccab15acd5858419d2432eec54c49a5
andy <andy@debian>
2016-04-21 17:02:01 -0400

    Made function to generate a random wallet and provide multiple receive addresses for it

--- Bitcoin-QR-Tools/src/com/starflask/BitcoinQrTools.java
+++ Bitcoin-QR-Tools/src/com/starflask/BitcoinQrTools.java
@@ -8,6 +8,9 @@ import org.bitcoinj.core.NetworkParameters;
 import org.bitcoinj.core.Wallet;
 import org.bitcoinj.crypto.DeterministicKey;
 import org.bitcoinj.params.MainNetParams;
+import org.bitcoinj.wallet.DeterministicSeed;
+
+import com.google.common.base.Joiner;
 
 import net.glxn.qrgen.core.image.ImageType;
 import net.glxn.qrgen.javase.QRCode;
@@ -17,25 +20,39 @@ public class BitcoinQrTools {
 
 	public static void main(String[] args) {
 		 
-		generateFreshKey();
-
+		generateSeededWalletWithAddresses(10000);
+		
 	}
 	
-	public static void generateFreshKey()
+	public static void generateSeededWalletWithAddresses(int numAddresses)
 	{
 		NetworkParameters params = new MainNetParams();
 		Wallet wallet = new Wallet( params );
+		
+		
+		
+		DeterministicSeed seed = wallet.getKeyChainSeed();
+		System.out.println("Seed words are: " + Joiner.on(" ").join(seed.getMnemonicCode()));
+		System.out.println("Seed birthday is: " + seed.getCreationTimeSeconds());
+
+		/*String seedCode = "yard impulse luxury drive today throw farm pepper survey wreck glass federal";
+		long creationtime = 1409478661L;
+		DeterministicSeed newseed = new DeterministicSeed(seedCode, null, "", creationtime);
+		Wallet restoredWallet = Wallet.fromSeed(params, newseed);
+		*/
+		
 		
 		DeterministicKey key = wallet.freshReceiveKey() ;
 		System.out.println( key.getPrivateKeyAsHex() );
 		
 		
-		
+		for(int i=0; i< numAddresses; i++)
+		{
 		Address address = wallet.freshReceiveAddress();
-		System.out.println(address.toString());
-		
+		System.out.println(address.toString() + ",");
+		}
 		 
-			
+		/*	
 		// get QR file from text using defaults
 		QRCode code = QRCode.from( address.toString() ) ;
 		
@@ -47,7 +64,7 @@ public class BitcoinQrTools {
 		
 		code.to(ImageType.PNG).writeTo(pngStream); //send this to a opengl texture 
 		
-		
+		*/
 		
 		 
 		
